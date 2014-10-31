1b4672b50e63590545bd32d6a55567935c7f3b0f
Xiangxi Gao <gaomrx@gmail.com>
2014-10-31 18:21:44 -0400

    Update artificial_data.py

--- artificial/artificial_data.py
+++ artificial/artificial_data.py
@@ -2,8 +2,11 @@ import random
 
 # create artificial DNA strings of length k 
 def randomDNA(k):
-  dna = ''
-  for i in range(k):
-    dna = dna + random.choice('ATCG')
-  return dna
+	dna = ''
+	for i in range(k):
+		dna = dna + random.choice('ATCG')
+		
+	return dna
+
+
   
