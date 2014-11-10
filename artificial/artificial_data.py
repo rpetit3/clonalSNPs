b276e6bebeb8823a7027b41223f54bf1108f7620
Xiangxi Gao <gaomrx@gmail.com>
2014-11-09 23:58:38 -0500

    Update artificial_data.py

--- artificial/artificial_data.py
+++ artificial/artificial_data.py
@@ -6,6 +6,18 @@ def randomDNA(k):
 	for i in range(k):
 		dna = dna + random.choice('ATCG')
 	return dna
+	
+# provide a string s and probability p that each base in s mutates and return a new string
+def mutateDNA(s,p):
+	new = ''
+	for i in range(len(s)):
+		prob = random.random()
+		if prob <= p:
+			new = new + random.choice('ATCG')
+		else:
+			new = new + s[i]
+	return new
+	
 
 
   
